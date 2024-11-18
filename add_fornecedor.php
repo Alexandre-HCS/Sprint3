5cc6792155f6d9cbc6bc962ed3f3870f1a6c99d9
Carlos Cerqueira <carlos.a.senai02@gmail.com>
2024-11-18 10:21:23 -0300

    Update add_fornecedor.php

--- add_fornecedor.php
+++ add_fornecedor.php
@@ -14,6 +14,37 @@ require 'conexao.php';
     <title>Dr.Peanuts</title>
 </head>
 <body>
+<h1>Adicionar Fornecedor</h1>    
+    <div class="add">
+        <form action="add_fornecedor" method="POST">
+        <label>Nome Fornecedor</label>
+        <input type="text" name="nomefornecedor" require>
+        
+        <label>CNPJ</label>
+        <input type="number" name="cnpj" require>
+        
+        <label>Endereço</label>
+        <input type="text" name="endereco">
+        
+        <label>Telefone</label>
+        <input type="number" name="telefone">
 
-</body>
+        <label>Email</label>
+        <input type="text" name="email" require>
+
+        <label>Observações</label>
+        <input type="text" name="observacoes">
+
+        <input type="submit" value="Adicionar Fornecedor">
+        </form>
+    </div>
+    <br>
+    <h2>Lista de fornecedores</h2>
+    <div>
+        <?php  include 'read_fornecedore.php'; 
+        
+        ?>
+    </div>
+        </body>
+
 </html>
